2bd5f6df94cfb421e51cd5c9776594ef49b04ec7
Mohit Bhasi <mohitbhasi1998@gmail.com>
2019-06-23 20:07:41 +0530

    Heroku deployment

--- website/website/settings.py
+++ website/website/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,13 +24,12 @@ AUTH_USER_MODEL = 'account.User'
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4-g=qyb!6+g+2th7dsutckd!(db33-%@y0j$-kel3fwh76bbtk'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['istenitk.herokuapp.com']
 
 # Application definition
 
