e91590014faf4be5339c8430bc05d85a02138aad
Mohit Bhasi <mohitbhasi1998@gmail.com>
2019-06-24 12:59:53 +0530

    Applicant model changed

--- account/models.py
+++ account/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
  
 def user_avatar_path(instance, filename):
     return 'user_{0}/avatar/{1}'.format(instance.id, filename)
  
 class User(AbstractUser):
-    phone_number=models.IntegerField(default=0)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     avatar=models.ImageField(upload_to=user_avatar_path)
     def __str__(self):
         return self.username
