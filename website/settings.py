59c6288a28e32dbe306cc28c98bc73c5d9d13a06
Mohit Bhasi <mohitbhasi1998@gmail.com>
2019-06-24 19:49:15 +0530

    uncessary libraries removed

--- website/settings.py
+++ website/settings.py
@@ -48,14 +48,12 @@ INSTALLED_APPS = [
     'recruitments',
     'blogs',
     #3rd Party
-    'django_admin_hstore_widget',
     'ckeditor',
     'ckeditor_uploader',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,7 +125,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
