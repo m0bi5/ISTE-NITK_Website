34e59978a22b8e7b47b8db8e19cd8fd8b71d620e
Amodh Shenoy <32415315+AmodhShenoy@users.noreply.github.com>
2019-06-28 12:17:06 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import dotenv
 
 
 def main():
-    #dotenv.read_dotenv()
+    dotenv.read_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
     try:
         from django.core.management import execute_from_command_line
