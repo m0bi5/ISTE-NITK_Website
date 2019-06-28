75323f113791bf28e9414e58dbaa1e5531a0d02c
Mohit Bhasi <mohitbhasi1998@gmail.com>
2019-06-28 14:21:29 +0530

    Projects done

--- projects/models.py
+++ projects/models.py
@@ -4,23 +4,12 @@ import uuid
 import datetime
 from account import models as account_models
 
-SIG_CHOICES=(
-    ('Crypt','CRYPT'),
-    ('Charge','CHARGE'),
-    ('Credit','CREDIT'),
-    ('Chronicle','CHRONICLE'),
-    ('Clutch','CLUTCH'),
-    ('Concrete','CONCRETE'),
-    ('Create','CREATE'),
-    ('Catalyst','CATALYST'))
-
 def year_choices():
     return [(r,r) for r in range(2018, datetime.date.today().year +1)]
 
 class Project(models.Model):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(default="",max_length=200)
     members = models.ManyToManyField(account_models.User)
-    sig = models.CharField(max_length=9, choices=SIG_CHOICES)
+    sig = models.ForeignKey(account_models.SIG,on_delete=models.CASCADE)
     year = models.IntegerField(choices=year_choices(),default=datetime.date.today().year)
     description = RichTextUploadingField()
--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
 from .models import Project
+from account import models as account_models
 import datetime
 # Create your views here.
 def projects(request):
@@ -9,9 +10,8 @@ def projects(request):
     for year in year_list:
         projects_list[year] = {}
         sigs = ['Crypt','Charge','Credit','Chronicle','Clutch','Concrete','Create','Catalyst']
-        for sig in sigs:
-            projects_list[year][sig] = Project.objects.filter(year=year,sig=sig)
-    print(projects_list)
+        for sig in account_models.SIG.objects.all():
+            projects_list[year][sig.name] = Project.objects.filter(year=year,sig=sig)
     return render(request,'projects/project_list.html',{'projects': projects_list})
 
 def show_project(request,id):
