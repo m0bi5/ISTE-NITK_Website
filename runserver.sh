fac932295b4e788adfddaffdd26437191efc5fec
Mohit Bhasi <mohitbhasi1998@gmail.com>
2019-08-02 16:21:46 +0530

    Configuring

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
-uwsgi --http :8000 --ini conf/iste_uwsgi.ini
+cp conf/iste_website.service /etc/systemd/system/iste_website.service
 cp conf/iste_nginx_conf /etc/nginx/sites-available
 rm /etc/nginx/sites-enabled/iste_nginx_conf
 ln -s /etc/nginx/sites-available/iste_nginx_conf /etc/nginx/sites-enabled
