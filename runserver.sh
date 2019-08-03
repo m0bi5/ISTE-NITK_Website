53a243385a7da40f0a4e5a26b1c26767b6ddacc3
Mohit Bhasi <mohitbhasi1998@gmail.com>
2019-08-03 11:48:43 +0530

    Email links fixed

--- runserver.sh
+++ runserver.sh
@@ -3,3 +3,17 @@ cp conf/iste_nginx_conf /etc/nginx/sites-available
 rm /etc/nginx/sites-enabled/iste_nginx_conf
 ln -s /etc/nginx/sites-available/iste_nginx_conf /etc/nginx/sites-enabled
  
+uid = iste
+base = /home/%(uid)
+
+chdir = %(base)/ISTE-NITK_Website
+#home = %(base)/projects/p1
+module = website.wsgi:application
+
+master = true
+processes = 5
+
+socket = /run/uwsgi/iste_website.sock
+chown-socket = %(uid):www-data
+chmod-socket = 660
+vacuum = true
