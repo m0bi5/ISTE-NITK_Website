5cb4258a04a28f018e5fe3f8e1e18caed2a34ac2
Mohit Bhasi <mohitbhasi1998@gmail.com>
2019-08-10 10:30:52 +0530

    User model updated;

--- account/models.py
+++ account/models.py
@@ -30,7 +30,7 @@ class User(AbstractUser):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
     avatar=models.ImageField(upload_to=user_avatar_path,blank=True)
-    sigs=models.ManyToManyField(SIG, editable=False)
+    sigs=models.ManyToManyField(SIG, editable=True)
     batch_of=models.IntegerField(default=2020, editable=True)
     def __str__(self):
         return self.first_name+' '+self.last_name
