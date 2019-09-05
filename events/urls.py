d9cf7867be77e1222902dbcfb058e2e2cd934b87
Mohit Bhasi <mohitbhasi1998@gmail.com>
2019-09-05 18:48:20 +0530

    room number added static

--- events/urls.py
+++ events/urls.py
@@ -5,5 +5,4 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.event_view, name='events'),
     path('register/<event_id>/', views.event_register, name='event_register'),
-    path('register/form_teams/<event_id>/<roll_no>/', views.event_form_team, name='event_form_team'),
 ] 
