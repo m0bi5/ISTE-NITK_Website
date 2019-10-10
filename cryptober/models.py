0937bad192890cf2698d9f9403323ee93c99c916
Amodh Shenoy <amodhshenoy@gmail.com>
2019-10-10 15:57:20 +0530

    Made changes to models and finalized view functions

--- cryptober/models.py
+++ cryptober/models.py
@@ -3,18 +3,12 @@ from ckeditor_uploader.fields import RichTextUploadingField
 import uuid
 import datetime
 
-class Team(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(default="",max_length=200)
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-    phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
-    email = models.EmailField()
-
 
 class Response(models.Model):
-    team = models.ForeignKey(Team,on_delete=models.CASCADE)
-    input1 = models.CharField(default="",max_length=60)
-    input2 = models.CharField(default="",max_length=60)
-    input3 = models.CharField(default="",max_length=60)
-    input4 = models.CharField(default="",max_length=60)
-    input5 = models.CharField(default="",max_length=60)
+    name = models.CharField(default="",max_length=200)
+    email = models.EmailField()
+    input1 = models.CharField(default="",max_length=100)
+    input2 = models.CharField(default="",max_length=100)
+    input3 = models.CharField(default="",max_length=100)
+    input4 = models.CharField(default="",max_length=100)
+    input5 = models.CharField(default="",max_length=100)
--- cryptober/views.py
+++ cryptober/views.py
@@ -1,21 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 
 # Create your views here.
 def instructions(request):
-    return render(request,'cryptober/instructions.html',{'team':team})
+    if request.method=='GET':
+        return render(request,'cryptober/instructions.html')
+    else:
+        name = request.POST.get('name').strip()
+        email = request.POST.get('email')
+        Response.objects.create(name=name,email=email)
+        return redirect(name+'/questions/1')
+
+def func1(query):
+    pass
+
+def func2(query):
+    pass
+
+def func3(query):
+    pass
+
+def func4(query):
+    pass
+
+def func5(query):
+    pass
 
 def questions(request,team,id):
-    response = Response.objects.get(team__id=id)
-    inputs = eval('response.input'+str(id)).strip().split()
+    response = Response.objects.get(name=team)
+    prev_inputs = eval(eval('response.input'+str(id)))
     if request.method=='GET':
-        return render(request,'cryptober/question'+str(id),{'inputs':inputs},'team':team)
+        return render(request,'cryptober/question'+str(id)+'.html',{'inputs':prev_inputs,'team':team})
     else:
-        if len(inputs)!=5:
-            query = request.POST.get('query',None)
+        if len(prev_inputs)<5:
+            query = request.POST.get('query',None).strip().split()
             output = eval('func'+str(id)+'(query)')
             if output==-6942069:
                 return render(request,'cryptober/error.html')
-            #add to the stored inputs
-            inputs = eval('response.input'+str(id)).strip().split()
-        return render(request,'cryptober/question'+str(id),{'inputs':inputs})
+            query.append(output)
+            eval('response.input'+str(id)+'=prev_inputs.append(query)')
+        return render(request,'cryptober/question'+str(id)+'.html',{'inputs':prev_inputs,'team':team})
