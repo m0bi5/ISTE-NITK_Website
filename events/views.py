3363ea3a41743897e6919eae60c7674efda5d5c3
Amodh Shenoy <amodhshenoy@gmail.com>
2019-10-26 13:44:43 +0530

    Update views.py

--- events/views.py
+++ events/views.py
@@ -28,7 +28,7 @@ def event_form_team(request,event_id,roll_no):
             else:
                 messages.error(request,"Ask your team mates to register for the event first!")
         else:
-            team=Team.objects.create(event=event,team_name=request.POST['team_name'],member1=roll_no)
+            team=Team.objects.create(event=event,team_name=request.POST['member1'],member1=roll_no)
             team.save()
             messages.success(request,"Team successfully created!")
                 
@@ -42,6 +42,8 @@ def event_register(request, event_id):
         form = None
         if event.members==3:
             form=ThreeForm(request.POST)
+        if event,members==1:
+            form=OneForm(request.POST)
         if event.members==4:
             form=FourForm(request.POST)
         if form.is_valid():
@@ -50,6 +52,8 @@ def event_register(request, event_id):
                 obj=ThreeMember(event=event,team_name=request.POST['team_name'],participant1=request.POST['participant1'],participant2=request.POST['participant2'],participant3=request.POST['participant3'],phone1=request.POST['phone1'],phone2=request.POST['phone2'],email=request.POST['email'])
             if event.members==4:
                 obj=FourMember(event=event,team_name=request.POST['team_name'],participant1=request.POST['participant1'],participant2=request.POST['participant2'],participant3=request.POST['participant3'],participant4=request.POST['participant4'],phone1=request.POST['phone1'],phone2=request.POST['phone2'],email=request.POST['email'])       
+            if event.members==1:
+                obj=OneMember(event=event,team_name=request.POST['participant1'],participant1=request.POST['participant1'],phone1=request.POST['phone1'],email=request.POST['email']) 
             obj.save()
             em_obj=em()
            # em_obj.send_email(request.POST['email'],"You have registered for ISTE NITK's "+event.event_name,"Hello "+request.POST['participant1']+"!\n Thank you for registering for "+event.event_name+" which will be held on "+str(event.event_date),'istenitkchapter@gmail.com','tqlsyhqfyskwutxh')
@@ -62,6 +66,8 @@ def event_register(request, event_id):
     else:
         if event.members==3:
             form=ThreeForm()
+        if event.members==1:
+            form=OneForm()
         if event.members==4:
             form = FourForm()
     context = {
