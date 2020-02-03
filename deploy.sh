14660ce981fa9f463510e6c6bf128762603175d8
Mohit Bhasi <mohitbhasi1998@gmail.com>
2020-02-04 04:46:36 +0530

    Webhook deployed

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 killall screen
 sudo git pull istenitk:#includeistenitk.h@https://github.com/m0bi5/ISTE-NITK_Website
 sudo pkill -9 -f manage.py
-echo "hi"
 sudo screen python3 manage.py runserver 0.0.0.0:80
