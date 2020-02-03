18e8a2c1d296caf65b27e69e9bf83a4636de4ba8
Mohit Bhasi <mohitbhasi1998@gmail.com>
2020-02-04 02:50:31 +0530

    Webhook initialised

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from home import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
     path('webhook/', views.webhook, name='webhook')
 ]
